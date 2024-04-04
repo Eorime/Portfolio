24118763dda880004eabd9af442996eada43c6da
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-04-04 18:14:26 +0400

    Add new project

--- src/pages/projects/react/ReactPage.jsx
+++ src/pages/projects/react/ReactPage.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import Navbar from "../../../components/navbar/Navbar";
-import { ReactContainer, ReactHeader, ReactProjectContainer } from "./style";
+import {
+  ReactContainer,
+  ReactHeader,
+  ReactProject,
+  ReactProjectContainer,
+  StyledLink,
+} from "./style";
 import Footer from "../../../components/footer/Footer";
+import ImageSearch from "../../../assets/images/project-screenshots/image-search.png";
 
 const ReactPage = () => {
   return (
     <ReactContainer>
       <Navbar />
-      <h1 style={{ textAlign: "center" }}>Coming Soon!</h1>
       <ReactHeader>React JS</ReactHeader>
-      <ReactProjectContainer></ReactProjectContainer>
+      <ReactProjectContainer>
+        <StyledLink to={"https://smallimagesearch.netlify.app"} target="_blank">
+          <ReactProject src={ImageSearch} />
+        </StyledLink>
+      </ReactProjectContainer>
       <Footer />
     </ReactContainer>
   );
--- src/pages/projects/react/style.js
+++ src/pages/projects/react/style.js
@@ -1,4 +1,10 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
 
 export const ReactContainer = styled.div`
   font-family: pixelFont, Arial;
